f1783569335d899553e39ab087b5f8cdad88321b
Han <157330660+OProcessing@users.noreply.github.com>
2024-04-21 14:33:04 +0900

    upload

--- src/TestFile.py
+++ src/TestFile.py
@@ -6,14 +6,9 @@ from PyQt5.QtGui import *
 from PyQt5 import uic
 from PyQt5.QtCore import *
 
-from_class = uic.loadUiType("Han_ws/PyQt/card_machine.ui")[0]
+from_class = uic.loadUiType("./gui/test.ui")[0]
 
 class Receiver(QThread) :
-    detected = pyqtSignal(bytes)
-    recvTotal = pyqtSignal(int)
-    changedTotal = pyqtSignal()
-    noCard = pyqtSignal()
-
     def __init__ (self, conn, parent=None) :
         super (Receiver, self).__init__(parent)
         self.is_running = False
@@ -25,14 +20,11 @@ class Receiver(QThread) :
         self.is_running = True
         while (self.is_running == True) :
             if self.conn.readable() :
-                res = self.conn.read_until(b'\n')
-                if len(res) > 0 :
-                    res = res[:-2]
-                    cmd = res[:2].decode()
-                    print("recv total")
-                    print(len(res))
-                    self.recvTotal.emit(res[3:].decode())
-
+                line = self.conn.read_until(b'\n')
+                if (len(line)) > 0 :
+                    line = line[:-2].decode()
+                    print(line)
+                    
     def stop (self) :
         print("recv stop")
         self.is_running = False
@@ -42,38 +34,28 @@ class WindowClass(QMainWindow, from_class) :
         super().__init__()
         self.setupUi(self)
 
-        self.uid = bytes(4)
-        self.conn = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=1)
+        self.conn = serial.Serial(port='COM3', baudrate=9600, timeout=1)
 
         self.recv = Receiver(self.conn)
         self.recv.start()
+        
+        self.btnread.clicked.connect(self.read)
+        self.btnwrite.clicked.connect(self.write)
 
-        self.recv.detected.connect(self.detected)
-        self.recv.recvTotal.connect(self.recvTotal)
+        self.recv.run()
 
-        self.timer = QTimer()
-        self.timer.setInterval(3000)
-        self.timer.start()
-
-    def send (self, command, data = 0) :
-        print ("send")
-        req_data = struct.pack('<4s16sc', self.uid, data.encode(), b'\n')
+    def read (self) :
+        print("read")
+        data = 'read'
+        req_data = struct.pack('<7sc',data.encode(),b'\n')
         self.conn.write(req_data)
-        return
-    
-    def detected(self, uid) :
-        print("detected")
-        self.uid = uid
-        self.timer.stop()
-        self.getTotal()
-        self.btnreset.setDisabled(False)
-        return
-    
-    def recvTotal(self, total) :
-        print("recvTotal")
-        self.linecharge.setText("")
-        self.linepayment.setText("")
-    
+
+    def write (self) :
+        print("write")
+        data = 'write'
+        req_data = struct.pack('<7sc',data.encode(),b'\n')
+        self.conn.write(req_data)
+
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
     myWindows = WindowClass()
